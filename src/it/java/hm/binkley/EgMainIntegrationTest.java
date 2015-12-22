65b4a5cde25534cdb467440dd27331eb7e0dde03
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2015-12-22 09:12:22 -0600

    Use testsets plugin.

--- src/it/java/hm/binkley/EgMainIntegrationTest.java
+++ src/it/java/hm/binkley/EgMainIntegrationTest.java
@@ -2,10 +2,13 @@ package hm.binkley;
 
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 public final class EgMainIntegrationTest {
     @Test
     public void shouldRunSlow()
             throws InterruptedException {
         new EgMain().slow();
+        assertThat(2).isEqualTo(2);
     }
 }
