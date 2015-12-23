08b30b72fde31ce6a7cc090e3ea80773a0c6888b
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2015-12-23 16:34:10 -0600

    Verify no warnings/errors for IT.

--- src/it/java/hm/binkley/RootControllerIntegrationTest.java
+++ src/it/java/hm/binkley/RootControllerIntegrationTest.java
@@ -1,8 +1,10 @@
 package hm.binkley;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.logging.LogLevel;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.TestRestTemplate;
 import org.springframework.boot.test.WebIntegrationTest;
@@ -13,6 +15,7 @@ import org.springframework.web.client.RestOperations;
 import java.net.URI;
 import java.text.ParseException;
 
+import static hm.binkley.NiceLoggingRule.niceLoggingRule;
 import static java.lang.String.format;
 import static org.hamcrest.Matchers.equalTo;
 
@@ -20,6 +23,11 @@ import static org.hamcrest.Matchers.equalTo;
 @SpringApplicationConfiguration(EgMain.class)
 @WebIntegrationTest({"server.port:0", "management.port:0"})
 public final class RootControllerIntegrationTest {
+    @Rule
+    public final NiceLoggingRule<LogLevel> niceLogging = niceLoggingRule(
+            "^(?<timestamp>\\d{4,4}-\\d{2,2}-\\d{2,2} \\d{2,2}:\\d{2,2}:\\d{2,2}\\.\\d{3,3}) +(?<level>%s) +",
+            LogLevel.values());
+
     private final RestOperations template = new TestRestTemplate();
 
     @Value("${local.server.port}")
