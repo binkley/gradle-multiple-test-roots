b7babdcb58339da3724d804549ebed0151c482a1
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2015-12-23 12:07:58 -0600

    Controller tests.

--- src/it/java/hm/binkley/RootControllerIntegrationTest.java
+++ src/it/java/hm/binkley/RootControllerIntegrationTest.java
@@ -2,18 +2,39 @@ package hm.binkley;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
 import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.util.JsonPathExpectationsHelper;
+import org.springframework.web.client.RestOperations;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.net.URI;
+import java.text.ParseException;
+
+import static java.lang.String.format;
+import static org.hamcrest.Matchers.equalTo;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(EgMain.class)
 @WebIntegrationTest({"server.port:0", "management.port:0"})
 public final class RootControllerIntegrationTest {
+    private final RestOperations template = new TestRestTemplate();
+
+    @Value("${local.server.port}")
+    private int port;
+
+    /** @todo Clumsy use of internal Spring test class */
     @Test
-    public void shouldRunSlow() {
-        assertThat(2).isEqualTo(2);
+    public void shouldGetRoot()
+            throws ParseException {
+        final String root = template.getForObject(uriFor("/"), String.class);
+        new JsonPathExpectationsHelper("$.message").
+                assertValue(root, equalTo("Hello, world!"));
+    }
+
+    private URI uriFor(final String path) {
+        return URI.create(format("http://localhost:%d/%s", port, path));
     }
 }
