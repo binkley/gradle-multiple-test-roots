42cf5f0b4191f20cf16750c8d49f27681c046d1e
Brian Oxley <boxley@thoughtworks.com>
2020-04-22 08:19:34 -0500

    Fix coverage

--- b/src/test/java/hm/binkley/BUnitTest.java
+++ b/src/test/java/hm/binkley/BUnitTest.java
@@ -2,8 +2,14 @@ package hm.binkley;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class BUnitTest {
     @Test
-    void should() {
+    void shouldTell() {
+        final var name = "BOB";
+        final var lib = new BUnit(name);
+
+        assertThat(lib.tell()).contains(name);
     }
 }
